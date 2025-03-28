790419c5eb271ad94ff306396b012dbb424558d9
aahmad-1 <aahmad.paturusii@gmail.com>
2025-03-28 15:37:08 +0200

    test commit

--- app/src/main/java/com/example/oopprojectwork/MainActivity.java
+++ app/src/main/java/com/example/oopprojectwork/MainActivity.java
@@ -31,3 +31,4 @@ public class MainActivity extends AppCompatActivity {
 // inshalllah
 // ahmad push with love <3
 // allah hu akbar
+//test
--- app/src/main/java/com/example/oopprojectwork/TrainingArea.java
+++
@@ -1,4 +0,0 @@
-package com.example.oopprojectwork;
-
-public class TrainingArea {
-}
